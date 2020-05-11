602b124c09d98544666b9cb32950af7179cd9ac4
guerin <vguerin@etu.isara.fr>
2020-05-11 16:48:44 +0200

    Update app.R

--- Mouche/app.R
+++ Mouche/app.R
@@ -5,14 +5,13 @@ ui <- fluidPage(
     
     titlePanel("Suivi d'un elevage de drosophiles"),
 
-    # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
             sliderInput("Nb",
                         "Nombre de mouches dans l'elevage :",
                         min = 1,
                         max = 1000,
-                        value = 30),
+                        value = 100),
             
             sliderInput("jour",
                     "Nombre de jours d'elevage :",
@@ -21,23 +20,24 @@ ui <- fluidPage(
                     value = 5)
             ),
     
-        # Show a plot of the generated distribution
         mainPanel(
-           plotOutput("loinorm")
-        )
+           plotOutput("loinorm"),
+           textOutput("nous")
+        ),
+    
     )
   )
 
  
-# Define server logic required to draw a histogram
 server <- function(input, output) {
 
     output$loinorm <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        hist(rnorm(input$Nb,mean=0.1+input$jour*0.05,sd=0.01), main = paste("Repartition des poids de l'elevage au jour",input$jour),col="black", xlab = "Poids en mg ", ylab = "Nb d'individu par tranche de poids ",)
+        hist(rnorm(input$Nb,mean=0.1+input$jour*0.05,sd=0.01), main = paste("Repartition des poids de l'elevage au jour",input$jour),col="grey", xlab = "Poids en mg ", ylab = "Nb d'individu par tranche de poids ",)
+    })
+    output$nous <- renderText({
+    "T DEMESSE & V GUERIN"
     })
 }
 
-# Run the application 
 shinyApp(ui = ui, server = server)
 
