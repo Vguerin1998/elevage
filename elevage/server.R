4660b988c157874082dd582ef213535aca0d5e10
guerin <vguerin@etu.isara.fr>
2020-05-13 12:15:03 +0200

    Update server.R

--- elevage/server.R
+++ elevage/server.R
@@ -1,10 +1,12 @@
 server <- function(input, output) {
+  base<-read.table("base.csv",header=TRUE,sep = ";",encoding = "UTF8")
   
   output$loinorm <- renderPlot({
     hist(rnorm(input$Nb,mean=0.1+input$jour*0.05,sd=0.01), main = paste("Repartition des poids de l'elevage au jour",input$jour),col="grey", xlab = "Poids en mg ", ylab = "Nb d'individu par tranche de poids ",)
   })
   output$nous <- renderText({
-    "Groupe 3"
+    "T DEMESSE & V GUERIN"
   })
   
 }
+
