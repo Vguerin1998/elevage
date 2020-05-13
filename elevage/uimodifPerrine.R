d9ef5df4f14c5e16d88878c85d2b58b786c5f4ac
Perrine98 <64475427+Perrine98@users.noreply.github.com>
2020-05-13 15:48:40 +0200

    Add files via upload

---
+++ elevage/uimodifPerrine.R
@@ -0,0 +1,44 @@
+base <-read.table("base.csv",header=TRUE,sep = ";")
+
+ui <- fluidPage(
+  
+  
+  titlePanel("Suivi d'un élevage de drosophiles"),
+  
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput("Nb",
+                  "Nombre de mouches dans l'élevage :",
+                  min = 1,
+                  max = 1000,
+                  value = 100),
+      
+      sliderInput("jour",
+                  "Nombre de jours d'élevage :",
+                  min=0,
+                  max = 10,
+                  value = 5),
+
+      sliderInput("stress",
+                  "Niveau de stress:",
+                  min=1,
+                  max = 7,
+                  value = 2),
+      
+      selectInput("espece", "Choisir une espece:",
+                  choices = base[,1])
+    ),
+    mainPanel(
+      plotOutput("loinorm"),
+      textOutput("nous"),
+      textOutput("Nbyeuxbleus"),
+      
+      runif(1, min=0, max=0.2)* Nbyeuxbleus,  
+      
+    
+   
+    )
+    
+  )
+)
+s
