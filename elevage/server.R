0760f1cd70fb6c3b2202b4a44b8005ad77101b8d
guerin <vguerin@etu.isara.fr>
2020-05-14 11:53:23 +0200

    Update server.R

--- elevage/server.R
+++ elevage/server.R
@@ -56,27 +56,23 @@ server <- function(input, output) {
     paste("Le nombre de mouches avec les yeux bleus dans la population est de :", round(runif(1,0,1)*input$Nb)," mouches")
   })
   
-  output$report <- downloadHandler({
-    # For PDF output, change this to "report.pdf"
-    filename = "rapport.html"
-    content = function(filename)
-      # Copy the report file to a temporary directory before processing it, in
-      # case we don't have write permissions to the current working dir (which
-      # can happen when deployed).
-    tempReport <- file.path(tempdir(), "rapport.Rmd")
-    file.copy("rapport.Rmd", "tempReport", overwrite = TRUE)
+  output$report <- downloadHandler(
+    filename = "report.html",
+    content = function(file) {
       
-      # Set up parameters to pass to Rmd document
+      
+      tempReport <- file.path(tempdir(), "report.Rmd")
+      file.copy("report.Rmd", tempReport, overwrite = TRUE)
+      
       params <- list(s = input$espece, j=input$jour,pop=input$Nb, poids=input$poids_moyen)
       
-      # Knit the document, passing in the `params` list, and eval it in a
-      # child of the global environment (this isolates the code in the document
-      # from the code in this app).
-      rmarkdown::render("rapport.Rmd", output_file = filename,
+      rmarkdown::render(tempReport, output_file = file,
                         params = params,
                         envir = new.env(parent = globalenv())
       )
-    })
+    }
+  )
 }
+
 
   
