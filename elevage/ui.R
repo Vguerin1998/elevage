8d7494cfe06355035747ff1dfa1a5fc8598dffd8
Perrine98 <perrine.truche@free.fr>
2020-05-14 09:01:38 +0200

    Update ui.R

--- elevage/ui.R
+++ elevage/ui.R
@@ -33,7 +33,9 @@ ui <- fluidPage(
       textOutput("poids"),
       textOutput("nbyb"),
       plotOutput("espece"),
+      img(src="drosophile.png", height = 200, width = 200),
       textOutput("nous")
+      
       )
   )
 )
